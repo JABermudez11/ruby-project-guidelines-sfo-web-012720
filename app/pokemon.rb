d4b427691cd653691b7a2b4bad515ce144531cb5
JABermudez11 <justinanbermudez@gmail.com>
2020-02-11 22:16:55 -0800

    fixing tables

--- app/pokemon.rb
+++ app/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon < ActiveRecord::Base
 end
 
 # need to select which pokemon you want to play as
-def pokemon_life 
+def pokemon_life
     bulbasaur = PokeApi.get(pokemon: 'bulbasaur').name
     charmander = PokeApi.get(pokemon: 'charmander').name
     squirtle = PokeApi.get(pokemon: 'squirtle').name
