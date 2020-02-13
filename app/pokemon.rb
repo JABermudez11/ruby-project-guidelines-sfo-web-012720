cdcf26e362055a787c3a129b9bb9854644e9170c
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 14:21:52 -0800

    changed attributes in pokemon table

--- app/pokemon.rb
+++ app/pokemon.rb
@@ -1,9 +1,3 @@
 class Pokemon < ActiveRecord::Base
     
 end
-
-# bulbasaur = PokeApi.get(pokemon: 'bulbasaur').name
-# charmander = PokeApi.get(pokemon: 'charmander').name
-# squirtle = PokeApi.get(pokemon: 'squirtle').name
-# pikachu = PokeApi.get(pokemon: 'pikachu').name
-# eevee = PokeApi.get(pokemon: 'eevee').name
