21ff613f22bf8db8dfec97742ecda19dcc12be66
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 14:26:30 -0800

    changed user association

--- app/user.rb
+++ app/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-    belongs_to :pokemon
-    has_many :friends
+    has_many :pokemon
+    has_many :battles, through: :pokemon
 
     # setting default value
-    after_initialize :init
+    # after_initialize :init
 
-    def init
-        self.level ||= 1
-    end
+    # def init
+    #     self.level ||= 1
+    # end
 
     # # the pokemon you choose in the beginning of the interface
     # def self.selected_pokemon
