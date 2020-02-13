3459dd822b94b10c6b0a482326d45618ff78fd05
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 11:09:25 -0800

    fixed migration

--- db/migrate/04_create_battles.rb
+++ db/migrate/04_create_battles.rb
@@ -1,9 +1,9 @@
-class CreateBattles < ActiveRecord::Migration[5.2]
+class CreateBattles < ActiveRecord::Migration[4.2]
     def change
         create_table :battles do |t|
             t.integer :user_id
             t.integer :poke_id
-            t.integer :friends_id
+            # t.integer :friends_id
             t.string :outcome
         end
     end
