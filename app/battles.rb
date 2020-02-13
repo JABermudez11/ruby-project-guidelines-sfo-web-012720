017ce165e30e976d5b141eba4f4d8af92c1c70a5
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 15:03:25 -0800

    created association between pokemon and battles

--- app/battles.rb
+++ app/battles.rb
@@ -1,4 +1,4 @@
-class Battles < ActiveRecord::Base
-    belongs_to :user
-    has_many :pokemon
+class Battle < ActiveRecord::Base
+    belongs_to :opponent, foreign_key: "opponent_id", class_name: "Pokemon"
+    belongs_to :user_poke, foreign_key: "user_poke_id", class_name: "Pokemon"
 end
