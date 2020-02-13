c6f50e9c6b26d1b1d3c60bd86e203962d997a52c
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 14:47:50 -0800

    updated association

--- app/pokemon.rb
+++ app/pokemon.rb
@@ -1,3 +1,10 @@
 class Pokemon < ActiveRecord::Base
+    belongs_to :users
+    # opponent
+    has_many :user_pokes, through: :user_battles, source: :user_poke
+    has_many :user_battles, foreign_key: :opponent_id, class_name: "Battle"
     
+    # user
+    has_many :opponents, through: :opponent_battles, source: :opponent
+    has_many :opponent_battles, foreign_key: :user_poke_id, class_name: "Battle"
 end
