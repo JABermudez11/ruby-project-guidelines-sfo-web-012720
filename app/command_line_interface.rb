250688bb299e44982882aee2407c0d10a594588c
JABermudez11 <justinanbermudez@gmail.com>
2020-02-12 17:14:01 -0800

    fixed who-are-you method

--- app/command_line_interface.rb
+++ app/command_line_interface.rb
@@ -19,7 +19,7 @@ def who_are_you
     choose_starter(starter)
     
     # create user instance
-    create_user(name, life)
+    create_user(name, starter)
     binding.pry
 end
 
@@ -33,19 +33,19 @@ def choose_starter(starter)
 
     case starter
     when "Pikachu"
-        puts "You have chosen Pikachu.."
+        puts "You have chosen Pikachu.. The Electric Mouse Pokemon"
         puts "Welcome to this world of Pokemon!"
     when "Bulbasaur"
-        puts "You have chosen Bulbasaur.."
+        puts "You have chosen Bulbasaur.. The Seed Pokemon"
         puts "Welcome to this world of Pokemon!"
     when "Charmander"
-        puts "You have chosen Charmander.."
+        puts "You have chosen Charmander.. The Fire Lizard Pokemon"
         puts "Welcome to this world of Pokemon!"
     when "Squirtle"
-        puts "You have chosen Squirtle.."
+        puts "You have chosen Squirtle.. The Turtle Pokemon"
         puts "Welcome to this world of Pokemon!"
     when "Eevee"
-        puts "You have chosen Eevee.."
+        puts "You have chosen Eevee.. The Evolution Pokemon"
         puts "Welcome to this world of Pokemon!"
     else
         puts "Please enter a valid name..."
