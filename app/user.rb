52520da36ed17dd529fd1f5d896c00be38a26f5f
JABermudez11 <justinanbermudez@gmail.com>
2020-02-13 23:39:06 -0800

    dont rememnber

--- app/user.rb
+++ app/user.rb
@@ -1,16 +1,4 @@
 class User < ActiveRecord::Base
     has_many :pokemon
     has_many :battles, through: :pokemon
-
-    # setting default value
-    # after_initialize :init
-
-    # def init
-    #     self.level ||= 1
-    # end
-
-    # # the pokemon you choose in the beginning of the interface
-    # def self.selected_pokemon
-    #     chosen
-    # end
 end
